e04970bb343a9f777c97aa6c1c0e3def50be2105
stanciudrg <stanciudragos1337@gmail.com>
2023-06-27 15:57:25 +0300

    Create JavaScript initial variables

---
+++ script.js
@@ -0,0 +1,7 @@
+// Variables
+
+let mainContainer = document.querySelector('.main-container');
+let gridsContainer = document.querySelector('.grids-container');
+let gridSquareX;
+let gridSquareY;
+let squareCount = 32;

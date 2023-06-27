0e142060ef8eb256b2819d4e3f0d9c139101e037
stanciudrg <stanciudragos1337@gmail.com>
2023-06-27 16:01:33 +0300

    Draw 32 x 32 grid squares within the gridsContainer
    The main loop draws 16 squares on the x axis, while the nested loop
    draws 16 squares on the y axis for each square on the x axis.

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ let gridsContainer = document.querySelector('.grids-container');
 let gridSquareX;
 let gridSquareY;
 let squareCount = 32;
+
+// The main loop draws 16 squares on the x axis, while the nested loop draws 16
+// on the y axis for each square on the x axis.
+
+for (let i = 0; i < squareCount; i++) {
+
+    gridSquareX = document.createElement('div');
+    gridSquareX.classList.add('x-grid-square');
+    gridsContainer.appendChild(gridSquareX);
+
+    for (let j = 0; j < squareCount; j++) {
+
+        gridSquareY = document.createElement('div');
+        gridSquareY.classList.add('y-grid-square');
+        gridSquareX.appendChild(gridSquareY)
+
+    };
+
+}
